5470e2bf05417cb435d812823202460d409f7131
Vladimir Maltsev <ya.24port@yandex.ru>
2020-08-20 19:52:09 +0700

    Перед отправкой 2

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,19 @@ let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.popup__button-close');
 let popupButton = popup.querySelector('.popup__button');
+let name = document.querySelector('.profile__name');
+let paragraph = document.querySelector('.profile__description');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-description');
+
 
 let popupToggle = function () {
   popup.classList.toggle('popup_opened');
+  nameInput.value=name.textContent;
+  jobInput.value=paragraph.textContent;
 }
 
-let closePopup = function () {
+let closePopup = function (event) {
   if (event.target !== event.currentTarget) return
   popupToggle();
 }
@@ -16,18 +23,13 @@ popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', closePopup);
 
+let formElement = document.querySelector('.popup__container');
 
-let inputs = document.querySelectorAll('input');
-
-
-let savePopup = function () {
-  let name = document.querySelector('.profile__name');
-  let paragraph = document.querySelector('.profile__description');
-  name.textContent = inputs[0].value;
-  paragraph.textContent = inputs[1].value;
-  closePopup();
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    name.textContent = nameInput.value;
+    paragraph.textContent = jobInput.value;
+    popupToggle();
 }
 
-popupButton.addEventListener('click', savePopup);
-
-
+formElement.addEventListener('submit', formSubmitHandler);
