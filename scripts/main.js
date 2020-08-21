ae8d2b918ae4dbe108466ba091ee101e52af8642
Vladimir Maltsev <ya.24port@yandex.ru>
2020-08-21 10:51:43 +0700

    перед отправкой 3

--- scripts/main.js
+++ scripts/main.js
@@ -4,24 +4,27 @@ let popupCloseButton = popup.querySelector('.popup__button-close');
 let popupButton = popup.querySelector('.popup__button');
 let name = document.querySelector('.profile__name');
 let paragraph = document.querySelector('.profile__description');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-description');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_description');
 
-
-let popupToggle = function () {
+let popupOpen = function () {
   popup.classList.toggle('popup_opened');
   nameInput.value=name.textContent;
   jobInput.value=paragraph.textContent;
 }
 
-let closePopup = function (event) {
-  if (event.target !== event.currentTarget) return
-  popupToggle();
+let popupClose = function () {
+  popup.classList.toggle('popup_opened');
 }
 
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-popup.addEventListener('click', closePopup);
+let close = function (event) {
+  if (event.target !== event.currentTarget) return
+  popupClose();
+}
+
+popupOpenButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+popup.addEventListener('click', close);
 
 let formElement = document.querySelector('.popup__container');
 
@@ -29,7 +32,7 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     name.textContent = nameInput.value;
     paragraph.textContent = jobInput.value;
-    popupToggle();
+    popupClose();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
